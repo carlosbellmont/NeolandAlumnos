c309ee1e96a8a4aef36d7b579eb651f21376c7e2
Alicia Moreno Alvarez <alimoralvarez@gmail.com>
2020-08-24 15:42:45 +0200

    Commit 1 : email valido

--- app/src/main/java/com/cbellmont/neoland/MainActivity.kt
+++ app/src/main/java/com/cbellmont/neoland/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.cbellmont.neoland
 
-import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     companion object {
         const val TAG_USUARIO = "TAG_USUARIO"
@@ -16,10 +18,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val viewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(application)).get(MainActivityViewModel::class.java)
+
         ivLogo.setImageResource(R.mipmap.logo_neoland)
         ivTexto.setImageResource(R.mipmap.texto_neoland)
 
-        cargarPreferencias()?.let {
+
+        viewModel.cargarEmailGuardado()?.let {
             if (it.isNotEmpty()){
                 cbRecordar.isChecked = true
             }
@@ -27,38 +32,17 @@ class MainActivity : AppCompatActivity() {
         }
 
         cbRecordar.setOnClickListener {
-            if (!cbRecordar.isChecked) borrarPreferencias()
+            if (!cbRecordar.isChecked) viewModel.borrarEmail()
         }
 
         bLogin.setOnClickListener {
-            if (editTextTextEmailAddress.text.isEmpty()){
-                Toast.makeText(this, "El email no debe estar vacio", Toast.LENGTH_LONG).show()
-            } else if (!editTextTextEmailAddress.text.contains("@") || !editTextTextEmailAddress.text.contains(".")) {
-                Toast.makeText(this, "El email debe contener una @ y un .", Toast.LENGTH_LONG).show()
-            } else {
+            if (viewModel.esEmailValido(editTextTextEmailAddress.text.toString())) {
                 Toast.makeText(this, "Todo correcto", Toast.LENGTH_LONG).show()
-                if (cbRecordar.isChecked){
-                    guardarPreferencias(editTextTextEmailAddress.text.toString())
-                }
-                // No debes poner un else borrarPreferencias() ya que de ese modo solamente sería posible borrar el email poniendo otro email correcto.
+
+            } else {
+               Toast.makeText(this,  viewModel.getErrorFromEmail(editTextTextEmailAddress.text.toString()), Toast.LENGTH_LONG).show()
             }
-        }
-    }
 
-    private fun borrarPreferencias(){
-        guardarPreferencias("")
-    }
-
-    private fun cargarPreferencias() : String? {
-        val sharedPref = getPreferences(Context.MODE_PRIVATE)
-        return sharedPref.getString(TAG_USUARIO, "")
-    }
-
-    private fun guardarPreferencias(usuario : String) {
-        val sharedPref = getPreferences(Context.MODE_PRIVATE)
-        with (sharedPref.edit()) {
-            putString(TAG_USUARIO, usuario)
-            commit()
         }
     }
 }
